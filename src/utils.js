ed44f9f5e981fec24a25cbf5ead3fa8643812710
Asad Marfani <asad_marfani@outlook.com>
2024-07-04 18:10:53 +0500

    Added some code.

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+const concatFn1 = (a, b) => {
+    return `${a} ${b}`
+}
+export const concatFn2 = (a, b) => {
+    return `${a} ${b}`
+}
+export const concatFn3 = (a, b) => {
+    return `${a} ${b}`
+}
+export const concatFn4 = (a, b) => {
+    return `${a} ${b}`
+}
+export const concatFn5 = (a, b) => {
+    return `${a} ${b}`
+}
+export const concatFn6 = (a, b) => {
+    return `${a} ${b}`
+}
+
+export default concatFn1;
